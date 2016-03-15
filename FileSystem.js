8cc5de7ebce2372473dffad05cb4c974e0b6c3bd
Angel Torres <thekamitorres@gmail.com>
2016-03-14 18:12:38 -0600

    nada

--- FileSystem.js
+++ FileSystem.js
@@ -1,29 +1,30 @@
 var fs = require('fs');
 
-// // Con especificación de codificación al incio
-// fs.readFile('./eventos.js', 'utf8', function (error, archivo) {
-// 	//En ambos imprimira el archivo correctamente
-// 	console.log(archivo)
-// 	console.log(archivo.toString())
-// })
+// Con especificación de codificación al incio
+fs.readFile('./eventos.js', 'utf8', function (error, archivo) {
+	//En ambos imprimira el archivo correctamente
+	console.log(archivo)
+	console.log(archivo.toString())
+})
 
-// // Sin especificacón de codificación al inicio
-// fs.readFile('./eventos.js', function (error, archivo) {
-// 	//Imprimirá valores hexadecimales
-// 	console.log(archivo)
-// 	//Imprimirá el archivo correctamente
-// 	console.log(archivo.toString())
-// })
+// Sin especificacón de codificación al inicio
+fs.readFile('./eventos.js', function (error, archivo) {
+	//Imprimirá valores hexadecimales
+	console.log(archivo)
+	//Imprimirá el archivo correctamente
+	console.log(archivo.toString())
+})
 
-//Trae las estadisiticas del archivo
-// fs.stat('./eventos.js', function(error, stat){
-// 	//Opcciones de informacion a visalizar
-// 	console.log(stat)
-// 	console.log(stat.isDirectory())
-// 	console.log(stat.isFile())
-// 	console.log(stat.atime)
-// })
+// Trae las estadisiticas del archivo
+fs.stat('./eventos.js', function(error, stat){
+	//Opcciones de informacion a visalizar
+	console.log(stat)
+	console.log(stat.isDirectory())
+	console.log(stat.isFile())
+	console.log(stat.atime)
+})
 
+// ============== STREAMS =======================================================
 
 var stream = fs.createReadStream('./eventos.js')
 
@@ -33,7 +34,7 @@ stream.on('data', function(chunk){
 	console.log(chunk)
 })
 
-//manda la informacion del stream directo a la consola
+// Manda la informacion del stream directo a la consola
 stream.pipe(process.stdout)
 
 stream.on('end', function(){
